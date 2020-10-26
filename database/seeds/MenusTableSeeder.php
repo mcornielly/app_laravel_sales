09bf5593fe9695c978309d9490f0ae75fe2ffbde
Miguel Cornielly <mcornielly@FACTORYHKA.LOCAL>
2020-10-26 10:46:46 -0400

    roles module with the edition of their permissions

---
+++ database/seeds/MenusTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Menu;
+use Illuminate\Database\Seeder;
+
+class MenusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Menu::truncate();
+
+        
+    }
+}
